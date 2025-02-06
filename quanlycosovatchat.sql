75d76b257f042fcf5da837a03f548259c366ea84
vtak1908 <vukiet252@gmail.com>
2025-02-06 14:00:21 +0700

    database

---
+++ quanlycosovatchat.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 20, 2025 lúc 03:00 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlycosovatchat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `assets`
+--
+
+CREATE TABLE `assets` (
+  `Id_Assets` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Img` varchar(200) NOT NULL,
+  `Type` varchar(50) NOT NULL,
+  `Status` varchar(200) NOT NULL DEFAULT 'Tốt',
+  `Location` varchar(200) NOT NULL,
+  `PurchaDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `assets`
+--
+
+INSERT INTO `assets` (`Id_Assets`, `Name`, `Img`, `Type`, `Status`, `Location`, `PurchaDate`) VALUES
+(26, 'Máy chiếu', 'maychieu.png', 'Điện tử', 'Tốt', 'Phòng 101', '2024-12-20'),
+(27, 'Bảng', 'bang.jpg', 'Nội thất', 'Tốt', 'Phòng 102', '2025-01-01'),
+(28, 'Máy tính', 'maytinh.png', 'Điện tử', 'Tốt', 'Phòng 405', '2025-01-09'),
+(29, 'Bàn học', 'banhoc.png', 'Nội thất', 'Đang sửa chữa', 'Phòng 405', '2025-01-20');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `maintenance`
+--
+
+CREATE TABLE `maintenance` (
+  `Id` int(10) NOT NULL,
+  `Id_Assets` int(10) NOT NULL,
+  `MaintenanceDate` date NOT NULL DEFAULT current_timestamp(),
+  `Description` text NOT NULL,
+  `MaintenanceStatus` varchar(100) NOT NULL DEFAULT 'Đang xử lý'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `maintenance`
+--
+
+INSERT INTO `maintenance` (`Id`, `Id_Assets`, `MaintenanceDate`, `Description`, `MaintenanceStatus`) VALUES
+(10, 29, '2025-01-20', 'Hỏng', 'Đang xử lý');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `Id_User` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(20) DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`Id_User`, `username`, `password`, `role`) VALUES
+(3, 'admin', 'admin', 'admin');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `assets`
+--
+ALTER TABLE `assets`
+  ADD PRIMARY KEY (`Id_Assets`);
+
+--
+-- Chỉ mục cho bảng `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Assets` (`Id_Assets`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id_User`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `assets`
+--
+ALTER TABLE `assets`
+  MODIFY `Id_Assets` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT cho bảng `maintenance`
+--
+ALTER TABLE `maintenance`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id_User` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD CONSTRAINT `maintenance_ibfk_1` FOREIGN KEY (`Id_Assets`) REFERENCES `assets` (`Id_Assets`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
